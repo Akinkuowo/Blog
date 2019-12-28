b5a85c2428f9093e7d0ac10afdb72e31d7e9b771
Akinkuowo <akinseun1990@gmail.com>
2019-12-28 22:25:17 +0100

    adding blog components

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './app.css';
+
+import 'bulma/css/bulma.css'
+
+
+
+
+
+const App = () => {
+		return (
+ 		<div className="App">
+			<h1>App component</h1>
+		</div>	
+		
+		);
+
+}
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import HomePage from './components/Home/HomePage';
+import AboutPage from './components/About/AboutPage';
+
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+
+
+ReactDOM.render(
+    
+    <BrowserRouter>
+        <div>
+            <NavBar  className="topbar topbar-inverse topbar-expand-md topbar-sticky"/>
+            <Route exact={true} path="/" component={App} />
+            <Route path="/home" component={HomePage} />
+            <Route path="/about" component={AboutPage} />
+        </div>
+    </BrowserRouter>,document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
